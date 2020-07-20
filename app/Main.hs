271185a39c343f620aaa1939be3a6214ea80cec7
quim <quim.casals@gmail.com>
2020-07-20 21:37:40 +0200

    Scratch defined formulas, use TPTP

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,6 @@ import LogicSymbols
 import UnifAlg
 import Data.Set as Set
 import UniSet
-import qualified QfreeForm as Qfree
-import qualified QForm as QF
--- import Data.Fix
-import Data.Functor.Foldable
-import Data.Tree (drawTree)
 
 
 constC = VarT "C"
@@ -24,14 +19,6 @@ eq = Equal f_of_g_of_cx f_of_g_of_yk
 
 pred_p = Pred "P" [g_of_cx, h_of_z]
 pred_q = Pred "Q" [h_of_yk, constK]
-lit_p = Fix (Qfree.Ltr (Pos pred_p))
-lit_q = Fix (Qfree.Ltr (Neg pred_q))
-lit_p' = Fix (QF.Ltr (Pos pred_p))
-lit_q' = Fix (QF.Ltr (Neg pred_q))
-randomFormula = Fix (Qfree.Or [lit_p,lit_q])
-randomFormula3 = Fix (Qfree.And [lit_p,lit_q])
-randomFormulaFinal = Fix (Qfree.And [randomFormula3,randomFormula3, randomFormula])
-randomFormula2 = Fix (QF.Qtfy [QF.Forall (Var "x")] (Fix (QF.And [lit_p',lit_q'])))
 
 (result, emptyValues) = span (\(_, candidates, _) -> (not.(Set.null)) candidates) (unify [eq])
 
@@ -47,7 +34,3 @@ main = do
         -- putStrLn (show $ Prelude.map (\(x,_,_) -> x) computation)
         -- putStrLn (show $ Prelude.map (\(_,y,_) -> y) computation)
         -- putStrLn (show $ Prelude.map (\(_,_,z) -> z) computation)
-        -- putStrLn (show $ cata Qfree.toTree $ cata (Qfree.distribute randomFormula3 Qfree.makeOr) randomFormulaFinal)
-        -- putStrLn (drawTree $ cata Qfree.toTree randomFormulaFinal)
-        -- putStrLn (drawTree $ cata Qfree.toTree $ cata Qfree.distribute randomFormulaFinal)
-        -- putStrLn (show $ cata QF.toTree $ (cata QF.neg randomFormula2))
